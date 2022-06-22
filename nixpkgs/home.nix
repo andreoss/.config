22858da7ae65a0581ec03723b2adc78ef744c746
andreoss <andreoss@sdf.org>
2022-06-22 12:21:40 -0400

    Trim home.nix config

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -63,10 +63,9 @@ let
 in {
   nixpkgs.config = {
     packageOverrides = pkgs: {
-      nur = import (builtins.fetchTarball
-        "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-          inherit pkgs;
-        };
+      nur = import (builtins.fetchTarball {
+        url = https://github.com/nix-community/NUR/archive/master.tar.gz;
+      }) {inherit pkgs;};
     };
   };
   programs.direnv = {
