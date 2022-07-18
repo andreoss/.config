da3044e13f48f3363ecb6ce30e4fb54563e9e956
andreoss <andreoss@sdf.org>
2022-07-18 17:40:51 -0400

    Firefox policies.json

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -253,14 +253,92 @@ in {
   };
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox;
-    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-      browserpass
-      bukubrow
-      tridactyl
-      ublock-origin
-      umatrix
-    ];
+    package = pkgs.wrapFirefox pkgs.firefox-esr-unwrapped {
+      forceWayland = false;
+      extraPolicies = {
+        NoDefaultBookmarks = true;
+        DisableBuiltinPDFViewer = true;
+        DisableFeedbackCommands= true ;
+        DisableFirefoxAccounts= true ;
+        DisableFirefoxScreenshots= true ;
+        DisableFirefoxStudies= true ;
+        DisableForgetButton= true ;
+        DisableFormHistory= true ;
+        DisableMasterPasswordCreation= true ;
+        DisablePasswordReveal= true ;
+        DisablePocket = true;
+        DisablePrivateBrowsing= true ;
+        DisableProfileImport= true ;
+        DisableProfileRefresh= true ;
+        DisableSafeMode= true ;
+        DisableSetDesktopBackground= true ;
+        DisableSystemAddonUpdate= true ;
+        DisableTelemetry = true;
+        ManagedBookmarks = [];
+        Bookmarks = [];
+        "Extensions"= {
+          "Install"= [
+            "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin"
+            "https://addons.mozilla.org/firefox/downloads/latest/tridactyl-vim"
+            "https://addons.mozilla.org/firefox/downloads/latest/umatrix"
+            "https://addons.mozilla.org/firefox/downloads/latest/browserpass-ce"
+            "https://addons.mozilla.org/firefox/downloads/latest/decentraleyes"
+            "https://addons.mozilla.org/firefox/downloads/latest/flagfox"
+          ];
+        };
+        NetworkPrediction = false;
+        NewTabPage = false;
+        OfferToSaveLogins = false;
+        PasswordManagerEnabled = false;
+        SanitizeOnShutdown = true;
+        SearchEngines =  {
+          Default = "DuckDuckGo";
+          Remove = [ "Google" "Bing" "Amazon.com" ];
+          Add =  [
+            {
+              "Name" =  "Invidious";
+              "Description" =  "Search for videos, channels, and playlists on Invidious";
+              "URLTemplate" =  "https://invidious.snopyta.org/search?q={searchTerms}";
+              "Method" =  "GET";
+              "IconURL" =  "https://invidious.snopyta.org/favicon.ico";
+              "Alias" =  "invidious";
+            }
+            {
+              "Name" =  "GitHub";
+              "Description" =  "Search GitHub";
+              "URLTemplate" =  "https://github.com/search?q={searchTerms}";
+              "Method" =  "GET";
+              "IconURL" =  "https://github.com/favicon.ico";
+              "Alias" =  "github";
+            }
+            {
+              "Name" =  "Wikipedia (es)";
+              "Description" =  "Wikipedia (es)";
+              "URLTemplate" =  "https://es.wikipedia.org/w/api.php?action=opensearch&amp;format=xml&amp;search={searchTerms}&amp;namespace=100|104|0";
+              "Method" =  "GET";
+              "IconURL" =  "https://es.wikipedia.org/static/favicon/wikipedia.ico";
+              "Alias" =  "wikipedia-es";
+            }
+            {
+              "Name" =  "NixOS options";
+              "Description" =  "Search NixOS options by name or description.";
+              "URLTemplate" =  "https://search.nixos.org/options?query={searchTerms}";
+              "Method" =  "GET";
+              "IconURL" =  "https://nixos.org/favicon.png";
+              "Alias" =  "nixos-options";
+            }
+            {
+              "Name" =  "NixOS packages";
+              "Description" =  "Search NixOS options by name or description.";
+              "URLTemplate" =  "https://search.nixos.org/packages?query={searchTerms}";
+              "Method" =  "GET";
+              "IconURL" =  "https://nixos.org/favicon.png";
+              "Alias" =  "nixos-packages";
+            }
+          ];
+        };
+      };
+    };
     profiles."default" = {
       id = 0;
       extraConfig = builtins.readFile (builtins.fetchurl
@@ -268,8 +346,8 @@ in {
       settings = {
         "accessibility.force_disabled" = 1;
         "browser.bookmarks.autoExportHTML" = true;
-        "browser.bookmarks.file" = builtins.toString ~/.config/bookmarks.html;
-        "browser.bookmarks.restore_default_bookmarks" = false;
+        "browser.bookmarks.file" = builtins.toString ../bookmarks.html;
+        "browser.bookmarks.restore_default_bookmarks" = true;
         "browser.cache.disk.enable" = false;
         "browser.cache.offline.enable" = false;
         "browser.link.open_newwindow" = 2;
@@ -279,17 +357,8 @@ in {
         "browser.startup.homepage" = "about:blank";
         "browser.tabs.tabMinWidth" = 16;
         "browser.uidensity" = 1;
-        "devtools.application.enabled" = false;
-        "devtools.debugger.enabled" = false;
-        "devtools.inspector.enabled" = false;
-        "devtools.performance.enabled" = false;
-        "devtools.styleeditor.enabled" = false;
         "extensions.abuseReport.enabled" = false;
-        "extensions.pocket.enabled" = false;
-        "extensions.update.autoUpdateDefault" = false;
-        "extensions.update.enabled" = false;
         "full-screen-api.ignore-widgets" = true;
-        "pdfjs.disabled" = true;
         "privacy.clearOnShutdown.offlineApps" = true;
         "privacy.clearOnShutdown.siteSettings" = true;
         "privacy.cpd.offlineApps" = true;
