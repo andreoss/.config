30d8b399a120de40d1cc1f7d18534ea7d2c34fee
andreoss <andreoss@sdf.org>
2022-08-20 22:19:13 -0400

    Add davmail mail configuration module

---
+++ nixpkgs/mail-davmail.nix
@@ -0,0 +1,46 @@
+let
+  address = "";
+  domain = builtins.elemAt (builtins.split "@" address) 2;
+  name = "";
+in {
+  "${address}" = {
+    primary = false;
+    address = "${address}";
+    realName = "${name}"
+    userName = "${address}";
+    passwordCommand = "pass show w/${domain}/${address}";
+    imap = {
+      host = "127.0.0.1";
+      port = 1143;
+      tls = {
+        enable = false;
+      };
+    };
+    smtp = {
+      host = "localhost";
+      port = 1025;
+      tls = {
+        enable = true;
+        useStartTls = true;
+      };
+    };
+    mbsync = {
+      enable = true;
+      create = "maildir";
+      extraConfig.account = {
+        Timeout = 0;
+        AuthMechs = "LOGIN";
+      };
+    };
+    msmtp = {
+      enable = true;
+      extraConfig = {
+        passwordeval = "pass show w/${domain}/${address}";
+        user = "${address}";
+      };
+    };
+    notmuch = {
+      enable = true;
+    };
+  };
+}
