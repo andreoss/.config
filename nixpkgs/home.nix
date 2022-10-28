0454e85ddb90addef066d4f97a1b4e65f6809ba5
andreoss <andreoss@sdf.org>
2022-10-28 13:33:14 -0400

    Font for DPI

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -648,7 +648,7 @@ in {
     "Emacs*toolBar" = 0;
     "Emacs*menuBar" = 0;
     "Emacs*geometry" = "80x30";
-    "Emacs*font" = "-Xos4-Terminus-bold-normal-normal-*-32-*-*-*-m-*-iso10646-1";
+    "Emacs*font" = "Terminus";
     "Emacs*scrollBar" = "on";
     "Emacs*scrollBarWidth" = 6;
     "XTerm*charClass" = [ "37:48" "45-47:48" "58:48" "64:48" "126:48" ];
