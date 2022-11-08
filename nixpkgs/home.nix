0dc98d5a6d7314f75efa7efa91bab0e7c3436a2d
andreoss <andreoss@sdf.org>
2022-11-07 21:40:58 -0500

    Clean up clisp packages

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -3,14 +3,7 @@ let
   python3Plus = pkgs.python3.withPackages
     (ps: with ps; [ pep8 ipython pandas pip meson seaborn pyqt5 tkinter ]);
   python2Plus = pkgs.python27.withPackages (ps: with ps; [ pep8 pip ]);
-  sbclPackages = (with pkgs.lispPackages; [
-    dbus
-    external-program
-    bordeaux-threads
-    quicklisp
-    swank
-    stumpwm
-  ]) ++ (with pkgs; [ roswell sbcl clisp ]);
+  sbclPackages = (with pkgs; [ roswell sbcl clisp ]);
   jdkRelatedPackages = with pkgs; [
     android-tools
     heimdall
