a6f00fef4f434f647e4416213c75b1abfa7ecefb
andreoss <andreoss@sdf.org>
2022-11-07 22:08:01 -0500

    Initialize flake and lock file

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -852,7 +852,6 @@ in {
                    exit 1
                esac
                echo "Xft.dpi: $DPI" | ${pkgs.xorg.xrdb}/bin/xrdb -merge
-               systemctl --user restart conky.service
          '';
       };
     };
