d7799efb28ba399d762fa91b67965c242e0294da
andreoss <andreoss@sdf.org>
2022-11-16 22:08:46 -0500

    Add home manager nixos module

---
+++ os/hm-nixos.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, lib, stdenv, ... }:
+{
+  home.stateVersion = "22.05";
+  home.packages = with pkgs; [ nvi pciutils usbutils ];
+  xsession = {
+    enable = true;
+    scriptPath = ".xsession";
+    windowManager.command = ''
+       ${pkgs.icewm}/bin/icewm-session &
+       ${pkgs.feh}/bin/feh --no-fehbg --bg-center ${../wp/1.jpeg} &
+       while :
+       do
+          sleep 1m
+       done
+      wait
+    '';
+  };
+}
