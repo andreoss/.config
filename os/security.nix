5da825608ac8ce3e85cce2ce115916fbf995a85f
andreoss <andreoss@sdf.org>
2022-11-17 12:08:32 -0500

    Add security configuration module

---
+++ os/security.nix
@@ -0,0 +1,62 @@
+{ pkgs, self, ... }:
+let user = self.config.primaryUser.name;
+in {
+  services.usbguard = {
+    enable = true;
+    IPCAllowedUsers = [ user "root" ];
+    rules = builtins.readFile ./usb-whitelist;
+  };
+  programs.firejail = {
+    enable = true;
+    wrappedBinaries = { };
+  };
+  security.wrappers = {
+    firejail.source = "${pkgs.firejail.out}/bin/firejail";
+  };
+  security.lockKernelModules = true;
+  security.forcePageTableIsolation = true;
+  services.physlock.allowAnyUser = true;
+  services.physlock.enable = true;
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = true;
+    extraRules = [
+      {
+        users = [ user ];
+        commands = [{
+          command = "${pkgs.systemd}/bin/systemctl";
+          options = [ "NOPASSWD" ];
+        }];
+      }
+      {
+        users = [ user ];
+        commands = [{
+          command = "${pkgs.systemd}/bin/reboot";
+          options = [ "NOPASSWD" ];
+        }];
+      }
+      {
+        users = [ user ];
+        commands = [{
+          command = "${pkgs.systemd}/bin/halt";
+          options = [ "NOPASSWD" ];
+        }];
+      }
+      {
+        users = [ user ];
+        commands = [{
+          command = "${pkgs.procps}/bin/pkill";
+          options = [ "NOPASSWD" ];
+        }];
+      }
+      {
+        users = [ user ];
+        commands = [{
+          command = "${pkgs.coreutils}/bin/kill";
+          options = [ "NOPASSWD" ];
+        }];
+      }
+    ];
+    extraConfig = "Defaults lecture=never";
+  };
+}
