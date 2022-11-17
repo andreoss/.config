0c519e86eff3c5792fb9af27a53a7ebe8c5d0c58
andreoss <andreoss@sdf.org>
2022-11-16 22:08:46 -0500

    Add hardware module configuration

---
+++ os/hw.nix
@@ -0,0 +1,13 @@
+{ self, ... }:
+{
+  hardware.opengl.enable = true;
+  hardware.openrazer = {
+    enable = false;
+    users = [ self.config.primaryUser.name ];
+  };
+  services.haveged.enable = true;
+  programs.light.enable = true;
+  hardware.acpilight.enable = true;
+  services.acpid.enable = true;
+
+}
