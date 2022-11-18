409913cb3e27f7bf390e1108c26f21f8709d2342
andreoss <andreoss@sdf.org>
2022-11-18 16:37:28 -0500

    Format boot loader module

--- os/boot-loader.nix
+++ os/boot-loader.nix
@@ -1,5 +1,4 @@
-{ lib, config, pkgs, ... }:
-{
+{ lib, config, pkgs, ... }: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 2;
   boot.loader.efi.canTouchEfiVariables = false;
