2b4634ee23e9341e2fca1700aa8709ccefb89a9c
andreoss <andreoss@sdf.org>
2022-11-26 22:35:41 -0500

    run localyesconfig

--- overlays/kernel.nix
+++ overlays/kernel.nix
@@ -1,5 +1,5 @@
 final: prev: {
-  linux = prev.pkgs.linuxKernel.manualConfig {
+  linux = (prev.pkgs.linuxKernel.manualConfig {
     inherit (prev) stdenv lib;
     version = "6.1.0-rc6";
     configfile = ./.config;
@@ -8,6 +8,13 @@ final: prev: {
       url = "https://git.kernel.org/torvalds/t/linux-6.1-rc6.tar.gz";
       hash = "sha256-yW1r4F5h8iGK5F53QCW50/pJTRos1kMLnfJmlpsssew=";
     };
-  };
-  linuxPackages = prev.pkgs.recurseIntoAttrs (prev.pkgs.linuxPackagesFor final.linux);
+  }).overrideAttrs (attrs: {
+    postConfigure = ''
+       export PATH=$PATH:${prev.pkgs.kmod}/bin
+       make $makeFlags "''${makeFlagsArray[@]}" localyesconfig
+    '';
+
+  });
+  linuxPackages =
+    prev.pkgs.recurseIntoAttrs (prev.pkgs.linuxPackagesFor final.linux);
 }
