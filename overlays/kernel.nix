71feda1cab10c3daf41f609cb25fcef77e4e798f
andreoss <andreoss@sdf.org>
2022-11-28 21:19:23 -0500

    Simplify kernel overlay definition

--- overlays/kernel.nix
+++ overlays/kernel.nix
@@ -1,5 +1,5 @@
 final: prev: {
-  linux = (prev.pkgs.linuxKernel.manualConfig {
+  linux = prev.pkgs.linuxKernel.manualConfig {
     inherit (prev) stdenv lib;
     version = "6.1.0-rc6";
     configfile = ./.config;
@@ -8,13 +8,6 @@ final: prev: {
       url = "https://git.kernel.org/torvalds/t/linux-6.1-rc6.tar.gz";
       hash = "sha256-yW1r4F5h8iGK5F53QCW50/pJTRos1kMLnfJmlpsssew=";
     };
-  }).overrideAttrs (attrs: {
-    postConfigure = ''
-       export PATH=$PATH:${prev.pkgs.kmod}/bin
-       make $makeFlags "''${makeFlagsArray[@]}" localyesconfig
-    '';
-
-  });
-  linuxPackages =
-    prev.pkgs.recurseIntoAttrs (prev.pkgs.linuxPackagesFor final.linux);
+  };
+  linuxPackages = prev.pkgs.recurseIntoAttrs (prev.pkgs.linuxPackagesFor final.linux);
 }
