8ce26e9a372db3c43c4a25632841474b693073d8
andreoss <andreoss@sdf.org>
2022-11-28 21:19:23 -0500

    huge kernel

--- overlays/kernel.nix
+++ overlays/kernel.nix
@@ -2,7 +2,7 @@ final: prev: {
   linux = prev.pkgs.linuxKernel.manualConfig {
     inherit (prev) stdenv lib;
     version = "6.1.0-rc6";
-    configfile = ./.config;
+    configfile = ./config-huge;
     allowImportFromDerivation = true;
     src = prev.pkgs.fetchurl {
       url = "https://git.kernel.org/torvalds/t/linux-6.1-rc6.tar.gz";
