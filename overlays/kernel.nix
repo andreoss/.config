804faf41761c8c9dea4d3d4f49d1ceccd917a7cc
andreoss <andreoss@sdf.org>
2022-11-28 21:46:54 -0500

    bump

--- overlays/kernel.nix
+++ overlays/kernel.nix
@@ -1,12 +1,12 @@
 final: prev: {
   linux = (prev.pkgs.linuxKernel.manualConfig {
     inherit (prev) stdenv lib;
-    version = "6.1.0-rc6";
+    version = "6.1.0-rc7";
     configfile = ./config-huge;
     allowImportFromDerivation = true;
     src = prev.pkgs.fetchurl {
-      url = "https://git.kernel.org/torvalds/t/linux-6.1-rc6.tar.gz";
-      hash = "sha256-yW1r4F5h8iGK5F53QCW50/pJTRos1kMLnfJmlpsssew=";
+      url = "https://git.kernel.org/torvalds/t/linux-6.1-rc7.tar.gz";
+      hash = "sha256-5S9SY7BhSIux8aWREkZE93bwiG3sDSIB8bxMG9eNFJc=";
     };
   }).overrideAttrs (attrs: {
     postConfigure = ''
