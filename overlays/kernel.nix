ad6c7ab1547a1d59cc758ec2110a017ed3a99d40
andreoss <andreoss@sdf.org>
2022-12-05 16:39:30 -0500

    bump

--- overlays/kernel.nix
+++ overlays/kernel.nix
@@ -1,14 +1,15 @@
 final: prev: {
   linux = (prev.pkgs.linuxKernel.manualConfig {
     inherit (prev) stdenv lib;
-    version = "6.1.0-rc7";
+    version = "6.1.0-rc8";
     configfile = ./config-huge;
     allowImportFromDerivation = true;
-    src = prev.pkgs.fetchurl {
-      url = "https://git.kernel.org/torvalds/t/linux-6.1-rc7.tar.gz";
-      hash = "sha256-5S9SY7BhSIux8aWREkZE93bwiG3sDSIB8bxMG9eNFJc=";
+    src = prev.pkgs.fetchgit {
+      url = "git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git";
+      rev = "v6.1-rc8";
+      sha256 = "sha256-KSAotMNO3u+Y02txyCG7ax4YLt87Wwon6o2G8pMVZZY=";
     };
-  }).overrideAttrs (attrs: {});
+  }).overrideAttrs (attrs: { });
   linuxPackages =
     prev.pkgs.recurseIntoAttrs (prev.pkgs.linuxPackagesFor final.linux);
 }
