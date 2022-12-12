dcea2eef552bc43f2de8f320250d4ee37e472d0a
andreoss <andreoss@sdf.org>
2022-12-11 22:41:34 -0500

    Refactor iso image configuration

--- os/iso.nix
+++ os/iso.nix
@@ -1,45 +1,59 @@
 { config, inputs, lib, pkgs, modulesPath, ... }:
 let palette = import ./palette.nix;
-in
-{
-  imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
-  isoImage = {
-    efiSplashImage = ./../wp/1.jpeg;
-    splashImage = ./../wp/1.jpeg;
+in {
+  imports =
+    [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
+  config = {
+    isoImage = {
+      efiSplashImage = ./../wp/1.jpeg;
+      splashImage = ./../wp/1.jpeg;
+    };
+    boot.initrd.availableKernelModules = lib.mkForce [
+      "ahci"
+      "ehci_pci"
+      "rtsx_pci_sdmmc"
+      "sata_nv"
+      "sd_mod"
+      "uhci_hcd"
+      "usb_storage"
+      "xhci_pci"
+    ];
+    boot.supportedFilesystems =
+      lib.mkForce [ "btrfs" "vfat" "f2fs" "xfs" "ntfs" "ext4" ];
+
+    boot.initrd.verbose = false;
+    boot.plymouth = { enable = true; };
+    console = {
+      packages = [ pkgs.terminus_font ];
+      font = "ter-132n";
+      earlySetup = true;
+      useXkbConfig = true;
+      colors = builtins.map (x: builtins.replaceStrings [ "#" ] [ "" ] x)
+        (with palette; [
+          black1
+          red1
+          green1
+          yellow1
+          blue1
+          red3
+          cyan1
+          yellow2
+          black2
+          orange1
+          gray1
+          gray2
+          gray3
+          magenta
+          red3
+          white1
+        ]);
+    };
+    systemd.extraConfig = ''
+      DefaultTimeoutStartSec=10s
+      DefaultTimeoutStopSec=10s
+      DefaultOOMPolicy=kill
+      ShowStatus=error
+    '';
+    services.gpm.enable = lib.mkForce true;
   };
-  boot.initrd.verbose = false;
-  boot.plymouth = {
-    enable = true;
-  };
-  console = {
-    packages = [ pkgs.terminus_font ];
-    font = "ter-132n";
-    earlySetup =   true;
-    useXkbConfig = true;
-    colors = builtins.map (x: builtins.replaceStrings [ "#" ] [ "" ] x) (with palette; [
-      black1
-      red1
-      green1
-      yellow1
-      blue1
-      red3
-      cyan1
-      yellow2
-      black2
-      orange1
-      gray1
-      gray2
-      gray3
-      magenta
-      red3
-      white1
-    ]);
-  };
-  systemd.extraConfig = ''
-    DefaultTimeoutStartSec=10s
-    DefaultTimeoutStopSec=10s
-    DefaultOOMPolicy=kill
-    ShowStatus=error
-  '';
-  services.gpm.enable = lib.mkForce true;
 }
