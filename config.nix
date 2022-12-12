bb5cc9d00e0cea5de7965df4e0eda38c762e2fa9
andreoss <andreoss@sdf.org>
2022-12-11 22:41:34 -0500

    update config.nix

--- config.nix
+++ config.nix
@@ -1,43 +1,40 @@
 { lib, pkgs, config, ... }:
 with lib;
-let cfg = config.services.hello;
-in {
+{
   options.ao = mkOption { type = types.attrs; };
-  # stateVersion = mkOption { default = "22.11" ; type = types.str; };
-  # pipewireReplacesPulseaudio = mkOption {type = types.bool;};
-  # fileSystems = mkOption {type = types.attrs;};
-  # primaryUser = mkOption {type = types.attrs;};
-  #   {
-  #   name = "a";
-  #   home = "/user";
-  #   autoLogin = true;
-  #   emacsFromNix = true;
-  #   graphics = true;
-  #   mail = true;
-  #   languages = {
-  #     java = true;
-  #     perl = true;
-  #     scala = true;
-  #     android = false;
-  #   };
-  # };
-  #};
+  options.isLivecd = mkOption {type = types.bool; default = false ;};
+  options.mini = mkOption {type = types.bool; default = false ;};
+
   config.ao = {
     stateVersion = "22.11";
     fileSystems = { btrfsOptions = [ "compress=zstd" ]; };
     pipewireReplacesPulseaudio = true;
+    isLivecd = false;
     primaryUser = {
       name = "a";
+      uid = 1337;
+      passwd = "$6$FpbouABGBk53rccL$9.YA5q3qJOo0SHjJlZ.yjPjg.xczCkIHqJtcaeGbkt9N5//M60s8VzoTWhNy1FIPOQdT9aKGSgCv0GShLzDxo/";
+      handle = "andreoss";
+      email = "andreoss@sdf.org";
+      key = "2DB39B412CDF97C7";
       home = "/user";
       autoLogin = true;
       emacsFromNix = true;
       graphics = true;
       mail = true;
+      media = true;
+      office = true;
       languages = {
         java = true;
         perl = true;
         scala = true;
         android = false;
+        clojure = true;
+        rust = true;
+        ruby = true;
+        haskell = true;
+        lisp = true;
+        cxx  = true;
       };
     };
   };
