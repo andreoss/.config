5f122244706be45f7e081e8c611609f0ef40dcdf
andreoss <andreoss@sdf.org>
2022-12-15 17:38:22 -0500

    Update kernel and flake inputs

--- overlays/kernel.nix
+++ overlays/kernel.nix
@@ -1,13 +1,13 @@
 final: prev: {
   linux = (prev.pkgs.linuxKernel.manualConfig {
     inherit (prev) stdenv lib;
-    version = "6.1.0-rc8";
+    version = "6.1.0";
     configfile = ./config-huge;
     allowImportFromDerivation = true;
     src = prev.pkgs.fetchgit {
       url = "git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git";
-      rev = "v6.1-rc8";
-      sha256 = "sha256-KSAotMNO3u+Y02txyCG7ax4YLt87Wwon6o2G8pMVZZY=";
+      rev = "v6.1";
+      sha256 = "sha256-7HBzP6P/7KLCfKas4TRFfCutG0azFzV+IpQABtDMHnk=";
     };
   }).overrideAttrs (attrs: { });
   linuxPackages =
