6e04c92a89baca3c996a103a84a6a1a51165deb1
andreoss <andreoss@sdf.org>
2023-01-15 20:07:03 -0500

    Enhance perl development packages

--- hm/perl.nix
+++ hm/perl.nix
@@ -1,15 +1,29 @@
 { config, pkgs, lib, stdenv, self, ... }: {
   config = {
     home.packages = with pkgs.perl536Packages; lib.optionals(config.ao.primaryUser.languages.perl) [
-      ModernPerl
-      Moose
+      AnyEvent
       Appcpanminus
+      Appperlbrew
+      BUtils
+      Coro
+      DataDump
+      DBDSQLite
+      DBI
+      DBIxClass
+      FileUtil
+      HTMLTidy
+      JSON
+      ModernPerl
+      Mojolicious
+      Moose
+      NetSSLeay
       PerlCritic
       PerlTidy
       PodTidy
-      HTMLTidy
-      BUtils
-      Appperlbrew
+      TextCSV_XS
+      Tk
+      TryTiny
+      YAML
     ] ++ (with pkgs; [
       perl536
       rakudo
