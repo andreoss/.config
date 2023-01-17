f18c9a1f9ff9905064b30d2a790d0c33a026d7c3
andreoss <andreoss@sdf.org>
2023-01-16 20:36:28 -0500

    Update flake and kernel overlays

--- overlays/kernel.nix
+++ overlays/kernel.nix
@@ -1,12 +1,12 @@
 final: prev: {
   linux = (prev.pkgs.linuxKernel.manualConfig {
     inherit (prev) stdenv lib;
-    version = "6.1.0";
+    version = "6.2.0-rc4";
     configfile = ./config-huge;
     allowImportFromDerivation = true;
     src = prev.pkgs.fetchgit {
       url = "git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git";
-      rev = "v6.1";
+      rev = "v6.2-rc4";
       sha256 = "sha256-7HBzP6P/7KLCfKas4TRFfCutG0azFzV+IpQABtDMHnk=";
     };
   }).overrideAttrs (attrs: { });
