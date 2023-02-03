7e8f5fc81e388410de983ebf040eb2d1d1d037f9
andreoss <andreoss@sdf.org>
2023-02-02 22:38:46 -0500

    Add emacs configuration setup

--- hm/emacs.nix
+++ hm/emacs.nix
@@ -26,19 +26,90 @@
       '';
       extraPackages = elpa:
         with elpa; [
+          rg
+          project
+          sly
+          sly-asdf
+          sly-quicklisp
+          sly-repl-ansi-color
+          sly-macrostep
+          ack
+          bash-completion
           better-defaults
+          ccls
+          c-eldoc
+          centered-cursor-mode
+          cider
+          company
+          company-c-headers
+          company-posframe
+          dash
+          dashboard
+          default-text-scale
+          dired-subtree
+          editorconfig
+          eldoc
+          eldoc-cmake
           elfeed
+          elisp-format
+          elisp-lint
+          elisp-refs
+          elisp-slime-nav
           elpher
+          emms
+          eros
           evil
           evil-collection
+          evil-commentary
+          evil-exchange
+          evil-goggles
+          evil-snipe
           exwm
+          feebleline
+          flycheck
+          flymake-cursor
           forge
+          fringe-current-line
+          geiser-guile
+          general
+          git-gutter
           go-imports
+          guix
+          haskell-mode
+          hydra
+          lispy
+          lsp-haskell
+          lsp-java
+          lsp-metals
+          lsp-mode
+          lsp-ui
           magit
+          marginalia
+          writeroom-mode
+          nix-mode
+          notmuch
+          org
+          org-bullets
           pdf-tools
+          raku-mode
+          restart-emacs
+          sly
           telega
+          undo-fu
+          undo-tree
+          use-package
+          unicode-fonts
+          use-package-hydra
+          hl-todo
+          vertico-posframe
           vterm
+          which-key-posframe
+          winum
           xenops
+          prettify-greek
+          flyspell-correct
+          flyspell-correct-popup
+          rainbow-mode
         ];
     };
   };
