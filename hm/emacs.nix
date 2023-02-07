9807b519c663f0e59ad7afe84276e496a1a9f8c6
andreoss <andreoss@sdf.org>
2023-02-07 10:33:28 -0500

    Refactoring emacs configuration layout

--- hm/emacs.nix
+++ hm/emacs.nix
@@ -11,35 +11,34 @@
       "Emacs*toolBar" = 0;
       "Emacs*menuBar" = 0;
       "Emacs*geometry" = "80x30";
-      "Emacs*font" = "Terminus";
+      "Emacs*font" = "Ttyp0";
       "Emacs*scrollBar" = "on";
       "Emacs*scrollBarWidth" = 6;
     };
     services.emacs.enable = lib.mkForce false;
     programs.emacs = {
       enable = config.ao.primaryUser.emacsFromNix;
-      extraConfig = ''
-         (load-file "${inputs.emacs-d}/init.el")
-         (run-hooks (quote after-init-hook))
-         (run-hooks (quote emacs-startup-hook))
-         (run-hooks (quote window-setup-hook))
-      '';
+      extraConfig = builtins.readFile (pkgs.substituteAll {
+        src = ../init.el;
+        jc = inputs.jc-themes;
+        autofmt = inputs.elisp-autofmt;
+      });
       extraPackages = elpa:
-        with elpa; [
-          better-defaults
-          elfeed
-          elpher
-          evil
-          evil-collection
-          exwm
-          forge
-          go-imports
-          magit
-          pdf-tools
-          telega
-          vterm
-          xenops
-        ];
+        let
+          packageListNix =
+            pkgs.runCommand "init-packages.nix" { input = ../init.el; } ''
+              ${pkgs.perl}/bin/perl  -007 -nE '
+              BEGIN {
+                  say "{elpa, ...}: with elpa; [";
+                  say "use-package";
+              };
+              END   { say "]" };
+              while (m{[(]use-package \s* ([a-z-0-9]+) \s* (;\S+)?}xsgm) {
+                 next if $2 eq ";builtin";
+                 say $1;
+              }' "$input" >"$out" 
+            '';
+        in (import "${packageListNix}" { inherit elpa; });
     };
   };
 }
