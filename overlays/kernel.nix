b337f341afa0fa1ae708b5d550d216e3fac781a5
andreoss <andreoss@sdf.org>
2023-03-11 16:50:56 -0500

    update kernel

--- overlays/kernel.nix
+++ overlays/kernel.nix
@@ -1,12 +1,12 @@
 final: prev: {
   linux = (prev.pkgs.linuxKernel.manualConfig {
     inherit (prev) stdenv lib;
-    version = "6.2.0-rc4";
+    version = "6.3.0-rc1";
     configfile = ./config-huge;
     allowImportFromDerivation = true;
     src = prev.pkgs.fetchurl {
-      url = "https://git.kernel.org/torvalds/t/linux-6.2-rc4.tar.gz";
-      sha256 = "sha256-dyNCXhEjQbW953MoUVeB15NOq+2Dc/0UzIWtruly0PA";
+      url = "https://git.kernel.org/torvalds/t/linux-6.3-rc1.tar.gz";
+      sha256 = "sha256-oZhG9dYlRthT4TbRNuJ+/Kw/mRuGTIu2E9Dw5ge+xCo=";
     };
   }).overrideAttrs (attrs: { });
   linuxPackages =
