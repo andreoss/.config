5bce4d1cded7778ee7fe4ef0e9b20081479d129b
andreoss <andreoss@sdf.org>
2023-03-11 16:50:39 -0500

    mitigate mitm proxy behavior

--- os/audio.nix
+++ os/audio.nix
@@ -1,14 +1,28 @@
-{ lib, config , ... }: {
+{ lib, config, ... }:
+let
+  cfg = config.ao;
+  user = cfg.primaryUser.name;
+in {
   nixpkgs.config = {
     pulseaudio = true;
     mediaSupport = true;
   };
   hardware.bluetooth.enable = false;
   security.rtkit.enable = !config.ao.pipewireReplacesPulseaudio;
-  hardware.pulseaudio.enable = !config.ao.pipewireReplacesPulseaudio;
+  hardware.pulseaudio = {
+    enable = !config.ao.pipewireReplacesPulseaudio;
+    systemWide = true;
+    tcp = {
+      enable = true;
+      anonymousClients = {
+        allowedIpRanges = [ "127.0.0.1" "192.168.99.0/28" ];
+      };
+    };
+  };
   services.pipewire.enable = lib.mkForce config.ao.pipewireReplacesPulseaudio;
   services.pipewire.alsa.enable = config.ao.pipewireReplacesPulseaudio;
   services.pipewire.pulse.enable = config.ao.pipewireReplacesPulseaudio;
-  users.groups.pulse-access = { };
-  users.extraGroups.audio.members = [ config.ao.primaryUser.name ];
+  users.groups.audio.members = [ user ];
+  users.groups.pulse.members = [ user ];
+  users.groups.pulse-access.members = [ user ];
 }
