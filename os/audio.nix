6a06c17c8e2848974bd71705f17ef597a56f5f69
andreoss <andreoss@sdf.org>
2023-03-11 16:54:11 -0500

    Simplify audio module configuration

--- os/audio.nix
+++ os/audio.nix
@@ -1,24 +1,14 @@
-{ lib, config, ... }:
-let
-  cfg = config.ao;
-  user = cfg.primaryUser.name;
-in {
+{ lib, config , ... }: {
+  nixpkgs.config = {
+    pulseaudio = true;
+    mediaSupport = true;
+  };
   hardware.bluetooth.enable = false;
   security.rtkit.enable = !config.ao.pipewireReplacesPulseaudio;
-  hardware.pulseaudio = {
-    enable = !config.ao.pipewireReplacesPulseaudio;
-    systemWide = true;
-    tcp = {
-      enable = true;
-      anonymousClients = {
-        allowedIpRanges = [ "127.0.0.1" "192.168.99.0/28" ];
-      };
-    };
-  };
+  hardware.pulseaudio.enable = !config.ao.pipewireReplacesPulseaudio;
   services.pipewire.enable = lib.mkForce config.ao.pipewireReplacesPulseaudio;
   services.pipewire.alsa.enable = config.ao.pipewireReplacesPulseaudio;
   services.pipewire.pulse.enable = config.ao.pipewireReplacesPulseaudio;
-  users.groups.audio.members = [ user ];
-  users.groups.pulse.members = [ user ];
-  users.groups.pulse-access.members = [ user ];
+  users.groups.pulse-access = { };
+  users.extraGroups.audio.members = [ config.ao.primaryUser.name ];
 }
