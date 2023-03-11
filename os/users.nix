f1ac203dc96ca5ea3de53946ee57d8e4a02c66a1
andreoss <andreoss@sdf.org>
2023-03-11 16:50:39 -0500

    prune virtualisation option adjustments

--- os/users.nix
+++ os/users.nix
@@ -12,7 +12,7 @@
     home = config.ao.primaryUser.home;
   };
   users.groups = {
-    uinput  = { };
+    uinput = { };
     tunnel = { };
   };
   users.groups.wheel.members = [ config.ao.primaryUser.name ];
@@ -23,21 +23,18 @@
   services.logind.lidSwitch = "suspend";
   services.logind.extraConfig = "";
   programs.bash = {
-    promptInit =
-      ''
-     ${builtins.readFile ../shrc}
-     ${builtins.readFile ../bashrc}
-     '';
+    promptInit = ''
+      ${builtins.readFile ../shrc}
+      ${builtins.readFile ../bashrc}
+    '';
   };
   programs.zsh = {
     enable = true;
     enableBashCompletion = true;
     enableCompletion = true;
-    autosuggestions = {
-      enable = true;
-    };
+    autosuggestions = { enable = true; };
     promptInit = ''
-     ${builtins.readFile ../zshrc}
+      ${builtins.readFile ../zshrc}
     '';
   };
   environment = {
@@ -59,6 +56,11 @@
       psmisc
       stdmanpages
       wpa_supplicant_gui
+      links2
+      fbterm
+      jfbpdf
+      fbida
+      molly-guard
     ];
     shellAliases = { };
     homeBinInPath = true;
@@ -72,6 +74,7 @@
       [ -d "$HOME/.nix-profile" ] || /nix/var/nix/profiles/per-user/$USER/home-manager/activate &> /dev/null
     '';
   };
+  programs.nix-ld.enable = true;
   services.physlock = {
     enable = lib.mkForce true;
     allowAnyUser = true;
