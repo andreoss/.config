bf330a49b1eeb9664b96f00e92ac18495c62da14
andreoss <andreoss@sdf.org>
2023-03-12 09:46:33 -0400

    Remove youtube-dl config entries

--- hm/home.nix
+++ hm/home.nix
@@ -211,19 +211,7 @@ in {
       default-fg = palette.black1;
     };
   };
-  programs.yt-dlp = {
-    enable = desk;
-    settings = {
-      convert-subs = "srt";
-      downloader-args = "aria2c:'-c -x8 -s8 -k1M'";
-      downloader = "aria2c";
-      embed-metadata = true;
-      embed-subs = true;
-      embed-thumbnail = true;
-      mtime = true;
-      sub-langs = "all";
-    };
-  };
+  programs.yt-dlp.enable = desk;
   programs.home-manager.enable = true;
   programs.aria2 = {
     enable = !config.mini && config.ao.primaryUser.media;
