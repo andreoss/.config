3fa52c8237c67919b9b5d395d0a914132bf880ea
andreoss <andreoss@sdf.org>
2023-03-15 09:13:48 -0400

    add cachix

--- os/nix.nix
+++ os/nix.nix
@@ -2,6 +2,10 @@
   nix = {
     settings.experimental-features = [ "nix-command" "flakes" ];
     settings.trusted-users = [ "root" config.ao.primaryUser.name ];
+    settings.substituters = [ "https://kernel-overlay.cachix.org" ];
+    settings.trusted-public-keys = [
+      "kernel-overlay.cachix.org-1:rUvSa2sHn0a7RmwJDqZvijlzZHKeGvmTQfOUr2kaxr4="
+    ];
     settings.auto-optimise-store = true;
     settings.build-cores = 4;
     settings.require-sigs = true;
