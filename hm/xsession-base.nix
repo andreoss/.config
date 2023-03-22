90ff70df324ee7e7931bd0d532f6108fa2d53c3b
andreoss <andreoss@sdf.org>
2023-03-21 23:48:19 -0400

    cleanup repetitions

--- hm/xsession-base.nix
+++ hm/xsession-base.nix
@@ -3,16 +3,28 @@ let
   palette = import ../os/palette.nix;
   font = "Terminus";
 in {
+  options = { };
   config = {
-    programs.feh.enable = true;
+    home.pointerCursor = {
+      package = pkgs.openzone-cursors;
+      name = "OpenZone_White";
+      x11.enable = config.xsession.enable;
+      x11.defaultCursor = "left_ptr";
+    };
+    home.activation = lib.mkIf config.services.sxhkd.enable {
+      sxhkdUpdate = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        ${pkgs.procps}/bin/pgrep sxhkd && ${pkgs.procps}/bin/pkill -USR1 sxhkd
+      '';
+    };
+    programs.feh.enable = config.xsession.enable;
     programs.rofi = {
-      enable = true;
+      enable = config.xsession.enable;
       cycle = true;
       terminal = "urxvt";
       theme = "gruvbox-light-soft";
     };
     services.sxhkd = {
-      enable = true;
+      enable = config.xsession.enable;
       keybindings = {
         "alt + BackSpace" = "rofi -show-icons -show combi";
         "ctrl + alt + slash" = "rofi -show-icons -show filebrowser";
@@ -20,42 +32,42 @@ in {
           "${inputs.dmenu.packages.x86_64-linux.dmenu}/bin/dmenu_run";
       };
     };
-    home.activation.sxhkdUpdate = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      ${pkgs.procps}/bin/pgrep sxhkd && ${pkgs.procps}/bin/pkill -USR1 sxhkd
-    '';
-    services.dunst.enable = true;
-    services.dunst.settings = with palette; {
-      global = {
-        frame_color = black1;
-        separator_color = gray4;
-        transparency = 80;
-        font = "${font}";
-        alignment = "center";
-        word_warp = "true";
-        line_height = 3;
-        geometry = "384x5-30+20";
-      };
-      urgency_low = {
-        background = white3;
-        foreground = gray3;
-        timeout = 10;
-      };
-      urgency_normal = {
-        background = white4;
-        foreground = black1;
-        timeout = 20;
-      };
-      urgency_critical = {
-        background = red3;
-        foreground = black1;
-        timeout = 0;
+    services.dunst = {
+      enable = config.xsession.enable;
+      settings = with palette; {
+        global = {
+          frame_color = black1;
+          separator_color = gray4;
+          transparency = 80;
+          font = "${font}";
+          alignment = "center";
+          word_warp = "true";
+          line_height = 3;
+          geometry = "384x5-30+20";
+        };
+        urgency_low = {
+          background = gray5;
+          foreground = black0;
+          timeout = 10;
+        };
+        urgency_normal = {
+          background = gray5;
+          foreground = black0;
+          timeout = 20;
+        };
+        urgency_critical = {
+          background = white3;
+          foreground = black1;
+          frame_color = red1;
+          timeout = 0;
+        };
       };
     };
-    fonts.fontconfig.enable = true;
+    fonts.fontconfig.enable = config.xsession.enable;
     gtk = {
+      enable = config.xsession.enable;
       font.package = pkgs.terminus_font_ttf;
       font.name = "${font} 9";
-      enable = true;
       iconTheme = {
         name = "Adwaita";
         package = pkgs.gnome.adwaita-icon-theme;
@@ -81,10 +93,10 @@ in {
       ];
     };
     qt = {
-      enable = config.ao.primaryUser.graphics;
+      enable = config.xsession.enable;
       style.package = pkgs.adwaita-qt;
     };
-    home.packages = with pkgs; [
+    home.packages = lib.optionals config.xsession.enable (with pkgs; [
       noto-fonts-emoji
       noto-fonts
       paratype-pt-mono
@@ -105,17 +117,30 @@ in {
       wmctrl
       rox-filer
       xdotool
-    ];
-    systemd.user.services.fehbg = {
-      Unit = {
-        Description = "fehbg";
-        PartOf = [ "graphical-session.target" ];
+    ]);
+    home.sessionVariables = lib.mkIf config.xsession.enable {
+      XDG_SESSION_PATH = "";
+      XDG_SESSION_DESKTOP = "";
+      XDG_SESSION_TYPE = "";
+      XDG_SESSION_CLASS = "";
+      XDG_SESSION_ID = "";
+      XDG_CURRENT_SESSION = "";
+      GDMSESSION = "";
+      DESKTOP_SESSION = "";
+    };
+    systemd.user.services = lib.mkIf config.xsession.enable {
+      fehbg = {
+        Unit = {
+          Description = "fehbg";
+          PartOf = [ "graphical-session.target" ];
+        };
+        Service = {
+          ExecStart =
+            "${pkgs.feh}/bin/feh --no-fehbg --bg-fill ${../wp/1.jpeg}";
+          Restart = "never";
+        };
+        Install = { WantedBy = [ "graphical-session.target" ]; };
       };
-      Service = {
-        ExecStart = "${pkgs.feh}/bin/feh --no-fehbg --bg-fill ${../wp/1.jpeg}";
-        Restart = "never";
-      };
-      Install = { WantedBy = [ "graphical-session.target" ]; };
     };
   };
 }
