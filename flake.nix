75769cf9f7ddf4eb8a22180180846b1153d46180
andreoss <andreoss@sdf.org>
2023-03-21 23:08:12 -0400

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -46,6 +46,18 @@
           overlays = [
             inputs.kernel-overlay.overlays.default
             inputs.emacs-d.overlays.default
+            (self: super:
+              let
+                nixpkgs-mesa = builtins.fetchTarball {
+                  url =
+                    "https://github.com/nixos/nixpkgs/archive/bdac777becdbb8780c35be4f552c9d4518fe0bdb.tar.gz";
+                  sha256 =
+                    "sha256:18hi3cgagzkrxrwv6d9yjazqg5q2kiacjn3hhb94j4gs6c6kdxrk";
+                };
+              in {
+                mesa_drivers =
+                  (import nixpkgs-mesa { inherit system; }).mesa_drivers;
+              })
           ];
         });
       baseSystem = host:
