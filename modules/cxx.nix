f3badebbe270127e2bf140473d32a1399763ec2d
andreoss <andreoss@sdf.org>
2023-03-22 11:35:51 -0400

    Add C++ development module

---
+++ modules/cxx.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, lib, stdenv, self, ... }:
+let cfg = config.home.development.cxx;
+in {
+  options = {
+    home.development.cxx = with lib; {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+      };
+      version = mkOption {
+        type = types.str;
+        default = "536";
+      };
+    };
+  };
+  config = {
+    home.file = { ".indent.pro".source = ./../indent.pro; };
+    home.packages = lib.optionals cfg.enable (with pkgs; [
+      autoconf
+      entr
+      automake
+      binutils
+      ccls
+      clang-analyzer
+      clang-tools
+      cling
+      cloc
+      cmake
+      cppcheck
+      cpplint
+      ctags
+      gcc
+      gcovr
+      gdb
+      gnumake
+      indent
+      lcov
+      pkg-config
+      strace
+      tinycc
+      valgrind
+    ]);
+  };
+}
