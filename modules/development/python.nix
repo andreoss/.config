16b9464813d07eea4fe6037630391adaa9e240e0
andreoss <andreoss@sdf.org>
2023-03-23 17:09:47 -0400

    Add python development setup

---
+++ modules/development/python.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, stdenv, self, ... }:
+let
+  cfg = config.home.development.rust;
+  python3Plus = pkgs.python3.withPackages
+    (ps: with ps; [ pep8 ipython pandas pip meson seaborn pyqt5 tkinter ]);
+  python2Plus = pkgs.python27.withPackages (ps: with ps; [ pep8 pip ]);
+in {
+  options = {
+    home.development.rust = with lib; {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+      };
+    };
+  };
+
+  config = {
+    programs.matplotlib.enable = cfg.enable;
+    home.packages = lib.optionals cfg.enable [ python3Plus python2Plus ];
+  };
+}
