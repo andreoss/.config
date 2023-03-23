499683c958c0e69ab9bd620158c459c2d7e558eb
andreoss <andreoss@sdf.org>
2023-03-23 17:09:47 -0400

    Remove go home module

--- hm/go.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, lib, stdenv, self, ... }: {
-  config = lib.attrsets.optionalAttrs (self.config.primaryUser.languages.go) {
-    programs.go.enable = true;
-    home.packages = with pkgs; [
-      gotools gocode
-    ];
-  };
-}
