220dcdcab930305b55911625646eae819782084b
andreoss <andreoss@sdf.org>
2023-03-23 22:00:45 -0400

    Adjust python configuration

--- modules/development/python.nix
+++ modules/development/python.nix
@@ -1,12 +1,12 @@
 { config, pkgs, lib, stdenv, self, ... }:
 let
-  cfg = config.home.development.rust;
+  cfg = config.home.development.python;
   python3Plus = pkgs.python3.withPackages
     (ps: with ps; [ pep8 ipython pandas pip meson seaborn pyqt5 tkinter ]);
   python2Plus = pkgs.python27.withPackages (ps: with ps; [ pep8 pip ]);
 in {
   options = {
-    home.development.rust = with lib; {
+    home.development.python = with lib; {
       enable = mkOption {
         type = types.bool;
         default = false;
