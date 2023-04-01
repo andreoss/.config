18c49393672273fd01e1d80595bc37b52aecbcd6
andreoss <andreoss@sdf.org>
2023-03-31 20:44:58 -0400

    openbsd loader

--- os/boot-loader.nix
+++ os/boot-loader.nix
@@ -1,5 +1,29 @@
 { lib, config, pkgs, ... }: {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 2;
-  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader = {
+    efi.canTouchEfiVariables = false;
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 2;
+      extraInstallCommands = with pkgs; ''
+        ${perl536}/bin/perl -i -pE 's/^title \s* N.*/title X/x; s/^version \s+ (\S+) \s+ (\S+).*/version $2/x' /boot/loader/entries/*
+      '';
+
+      extraFiles = {
+        "etc/boot.conf" = pkgs.writeText "boot.conf" ''
+          machine gop 3
+          set device sr0a
+        '';
+        "extra/bootx64.efi" = pkgs.fetchurl {
+          url = "https://ftp.openbsd.org/pub/OpenBSD/7.2/amd64/BOOTX64.EFI";
+          sha256 = "0v8dysfd30clpk9f8sg5157yycwpcw39lrrr7rm88y146icym8c8";
+        };
+      };
+      extraEntries = {
+        "unix.conf" = ''
+          title OpenBSD
+          efi /extra/bootx64.efi
+        '';
+      };
+    };
+  };
 }
