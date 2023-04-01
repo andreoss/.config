620344ec8cf8507d041c375def8b9829684768a8
andreoss <andreoss@sdf.org>
2023-03-31 20:43:30 -0400

    grub without device

--- os/boot-grub.nix
+++ os/boot-grub.nix
@@ -1,15 +1,15 @@
 { lib, config, pkgs, ... }: {
-  boot.loader = {
-
-    grub = {
-      device = "/dev/sdb";
-      efiSupport = false;
-      enableCryptodisk = true;
-      enable = true;
-      splashImage = ../wp/1.jpeg;
-      splashMode = "normal";
-      theme = null;
-      version = 2;
+  config = {
+    boot.loader = {
+      grub = {
+        efiSupport = false;
+        enableCryptodisk = true;
+        enable = true;
+        splashImage = ../wp/1.jpeg;
+        splashMode = "normal";
+        theme = null;
+        version = 2;
+      };
     };
   };
 }
