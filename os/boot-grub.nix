b0d0d5dbca6058b52d5d2fc87a0462ac63257164
andreoss <andreoss@sdf.org>
2023-04-01 12:27:06 -0400

    Reformat grub boot config

--- os/boot-grub.nix
+++ os/boot-grub.nix
@@ -1,15 +1,15 @@
 { lib, config, pkgs, ... }: {
-  config = {
-    boot.loader = {
-      grub = {
-        efiSupport = false;
-        enableCryptodisk = true;
-        enable = true;
-        splashImage = ../wp/1.jpeg;
-        splashMode = "normal";
-        theme = null;
-        version = 2;
-      };
+  boot.loader = {
+
+    grub = {
+      device = "/dev/sdb";
+      efiSupport = false;
+      enableCryptodisk = true;
+      enable = true;
+      splashImage = ../wp/1.jpeg;
+      splashMode = "normal";
+      theme = null;
+      version = 2;
     };
   };
 }
