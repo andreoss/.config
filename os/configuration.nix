0e32a8d72fd638c9da7e8cc502aea0606d90abf0
andreoss <andreoss@sdf.org>
2023-04-08 16:46:08 -0400

    Refactor main system configuration

--- os/configuration.nix
+++ os/configuration.nix
@@ -1,35 +1,38 @@
 { lib, config, pkgs, self, ... }: {
-  system.copySystemConfiguration = false;
-  system.autoUpgrade = {
-    enable = false; # does not work with git-crypt
-    allowReboot = false;
-    dates = "01:00";
-    randomizedDelaySec = "10min";
-    flake = "github:andreoss/.config";
+  system = {
+    copySystemConfiguration = false;
+    autoUpgrade = {
+      enable = false; # does not work with git-crypt
+      allowReboot = false;
+      dates = "01:00";
+      randomizedDelaySec = "10min";
+      flake = "github:andreoss/.config";
+    };
   };
-  services.getty.extraArgs =
-    [ "--nohostname" "--noissue" "--noclear" "--nohints" ];
-  services.cron.systemCronJobs = [ "0 2 * * * root fstrim /" ];
-  systemd.oomd.enable = true;
-  services.earlyoom = {
-    enable = true;
-    enableNotifications = true;
-    freeMemThreshold = 1;
+  services = {
+    getty.extraArgs = [ "--nohostname" "--noissue" "--noclear" "--nohints" ];
+    cron.systemCronJobs = [ "0 2 * * * root fstrim /" ];
+    earlyoom = {
+      enable = true;
+      enableNotifications = true;
+      freeMemThreshold = 1;
+    };
+    udisks2.enable = true;
+    snapper = {
+      configs = {
+        home = {
+          subvolume = "${config.ao.primaryUser.home}";
+          extraConfig = ''
+            ALLOW_USERS="${config.ao.primaryUser.name}"
+            TIMELINE_CREATE=yes
+            TIMELINE_CLEANUP=yes
+          '';
+        };
+      };
+    };
   };
   environment.etc."packages".text = builtins.toJSON
     (map (x: { "${x.name}" = x.meta or { }; })
       config.environment.systemPackages);
-  services.udisks2.enable = true;
-  services.snapper = {
-    configs = {
-      home = {
-        subvolume = "${config.ao.primaryUser.home}";
-        extraConfig = ''
-          ALLOW_USERS="${config.ao.primaryUser.name}"
-          TIMELINE_CREATE=yes
-          TIMELINE_CLEANUP=yes
-        '';
-      };
-    };
-  };
+  systemd.oomd.enable = true;
 }
