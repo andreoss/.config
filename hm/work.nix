fbda15fa83c6006b7bd8149ae38389f0f3422ee4
andreoss <andreoss@sdf.org>
2023-04-17 09:30:21 -0400

    avoid collision

--- hm/work.nix
+++ hm/work.nix
@@ -36,14 +36,5 @@
       ratpoison
     ];
     home.file = { ".local/bin/dates".source = ./../scripts/dates; };
-    xsession = {
-      enable = true;
-      windowManager.command = "ratpoison";
-    };
-    programs.password-store.settings = {
-      PASSWORD_STORE_DIR = "${inputs.password-store}";
-      PASSWORD_STORE_KEY = "0x9E5BD0B11B382411";
-      PASSWORD_STORE_CLIP_TIME = "60";
-    };
   };
 }
