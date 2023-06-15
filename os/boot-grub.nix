fcc866625612bb76c588ca146ca2cf4e121f12e0
andreoss <andreoss@sdf.org>
2023-06-15 13:05:50 -0400

    Update grub boot configuration

--- os/boot-grub.nix
+++ os/boot-grub.nix
@@ -2,13 +2,13 @@
   config = {
     boot.loader = {
       grub = {
+        configurationLimit = 5;
         efiSupport = false;
         enableCryptodisk = true;
         enable = true;
         splashImage = ../wp/1.jpeg;
         splashMode = "stretch";
         theme = null;
-        version = 2;
         gfxmodeBios = "1920x1080";
         gfxmodeEfi = "1920x1080";
         fontSize = 24;
