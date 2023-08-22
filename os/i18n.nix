e6aadc977162a3540831b52f9b024b2ee08b8117
andreoss <andreoss@sdf.org>
2023-08-22 16:24:06 -0400

    Simplify i18n locale config

--- os/i18n.nix
+++ os/i18n.nix
@@ -2,14 +2,9 @@
   services.xserver.layout = "us,ru";
   services.xserver.xkbOptions = "ctrl:nocaps,grp:shifts_toggle,compose:ralt";
   i18n = {
-    supportedLocales = [
-      "en_GB.UTF-8/UTF-8"
-      "es_ES.UTF-8/UTF-8"
-      "pt_PT.UTF-8/UTF-8"
-      "ru_RU.UTF-8/UTF-8"
-    ];
-    defaultLocale = "ru_RU.UTF-8";
-    extraLocaleSettings = { LC_MESSAGES = "en_GB.UTF-8"; };
+    supportedLocales = [ "en_GB.utf8" "es_ES.utf8" "pt_PT.utf8" "ru_RU.utf8" ];
+    defaultLocale = "ru_RU.utf8";
+    extraLocaleSettings = { LC_MESSAGES = "en_GB.utf8"; };
   };
   time.timeZone = "America/New_York";
   location.provider = "manual";
