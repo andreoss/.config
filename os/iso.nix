d4c3daacc42e595dc2b956361628b83d2012fcbb
andreoss <andreoss@sdf.org>
2023-08-31 19:59:31 -0400

    clean up

--- os/iso.nix
+++ os/iso.nix
@@ -1,28 +1,30 @@
-{ config, inputs, lib, pkgs, modulesPath, ... }:
+{ lib, pkgs, modulesPath, ... }:
 let palette = import ./palette.nix;
 in {
-  imports =
-    [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
+  imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
   config = {
     isoImage = {
       efiSplashImage = ./../wp/1.jpeg;
       splashImage = ./../wp/1.jpeg;
     };
-    boot.initrd.availableKernelModules = lib.mkForce [
-      "ahci"
-      "ehci_pci"
-      "rtsx_pci_sdmmc"
-      "sata_nv"
-      "sd_mod"
-      "uhci_hcd"
-      "usb_storage"
-      "xhci_pci"
-    ];
-    boot.supportedFilesystems =
-      lib.mkForce [ "btrfs" "vfat" "f2fs" "xfs" "ntfs" "ext4" ];
-
-    boot.initrd.verbose = false;
-    boot.plymouth = { enable = true; };
+    boot = {
+      initrd = {
+        verbose = false;
+        availableKernelModules = lib.mkForce [
+          "ahci"
+          "ehci_pci"
+          "rtsx_pci_sdmmc"
+          "sata_nv"
+          "sd_mod"
+          "uhci_hcd"
+          "usb_storage"
+          "xhci_pci"
+        ];
+      };
+      supportedFilesystems =
+        lib.mkForce [ "btrfs" "vfat" "f2fs" "xfs" "ntfs" "ext4" ];
+      plymouth = { enable = true; };
+    };
     console = {
       packages = [ pkgs.terminus_font ];
       font = "ter-132n";
