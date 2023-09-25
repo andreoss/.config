39ac863eab534194377124ae435fb57d562c1ed0
andreoss <andreoss@sdf.org>
2023-09-24 23:54:26 -0400

    librewolf

--- modules/web.nix
+++ modules/web.nix
@@ -1,5 +1,32 @@
 { config, pkgs, lib, inputs, ... }:
-let cfg = config.home.web;
+let
+  cfg = config.home.web;
+
+  mozilla-common-settings = {
+    "security.cert_pinning.enforcement_level" = 0; # User MITM allowed
+    "accessibility.force_disabled" = 1;
+    "browser.cache.disk.enable" = false;
+    "browser.cache.offline.enable" = false;
+    "browser.link.open_newwindow" = 2;
+    "browser.privatebrowsing.autostart" = true;
+    "browser.search.searchEnginesURL" = "";
+    "browser.startup.homepage" = "about:blank";
+    "browser.tabs.tabMinWidth" = 16;
+    "browser.uidensity" = 1;
+    "extensions.abuseReport.enabled" = false;
+    "full-screen-api.ignore-widgets" = true;
+    "privacy.clearOnShutdown.offlineApps" = true;
+    "privacy.clearOnShutdown.siteSettings" = true;
+    "privacy.cpd.offlineApps" = true;
+    "privacy.cpd.passwords" = true;
+    "privacy.cpd.siteSettings" = true;
+    "signon.rememberSignons" = false;
+    "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+    "font.default.x-western" = "Terminus";
+    "font.name.monospace.x-western" = "Terminus";
+    "font.name.sans-serif.x-western" = "Terminus";
+    "font.name.serif.x-western" = "Terminus";
+  };
 in {
   imports = [ ];
   options = {
@@ -43,9 +70,12 @@ in {
       };
       browserpass = {
         enable = cfg.enable && config.programs.password-store.enable;
-        browsers = [ "brave" "chromium" "firefox" ];
+        browsers = [ "brave" "chromium" "firefox" "librewolf" ];
       };
-      librewolf.enable = cfg.enable;
+      librewolf = {
+        enable = cfg.enable;
+        settings = mozilla-common-settings;
+      };
       chromium.enable = cfg.enable;
       chromium.package = pkgs.brave;
       firefox = lib.mkIf cfg.enable {
@@ -53,34 +83,7 @@ in {
         profiles."default" = {
           id = 0;
           extraConfig = builtins.readFile "${inputs.user-js}/user.js";
-          settings = {
-            "accessibility.force_disabled" = 1;
-            "browser.bookmarks.autoExportHTML" = false;
-            "browser.places.importBookmarksHTML" = true;
-            "browser.bookmarks.file" = builtins.toString ../bookmarks.html;
-            "browser.bookmarks.restore_default_bookmarks" = true;
-            "browser.cache.disk.enable" = false;
-            "browser.cache.offline.enable" = false;
-            "browser.link.open_newwindow" = 2;
-            "browser.privatebrowsing.autostart" = true;
-            "browser.search.searchEnginesURL" = "";
-            "browser.startup.homepage" = "about:blank";
-            "browser.tabs.tabMinWidth" = 16;
-            "browser.uidensity" = 1;
-            "extensions.abuseReport.enabled" = false;
-            "full-screen-api.ignore-widgets" = true;
-            "privacy.clearOnShutdown.offlineApps" = true;
-            "privacy.clearOnShutdown.siteSettings" = true;
-            "privacy.cpd.offlineApps" = true;
-            "privacy.cpd.passwords" = true;
-            "privacy.cpd.siteSettings" = true;
-            "signon.rememberSignons" = false;
-            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-            "font.default.x-western" = "Terminus";
-            "font.name.monospace.x-western" = "Terminus";
-            "font.name.sans-serif.x-western" = "Terminus";
-            "font.name.serif.x-western" = "Terminus";
-          };
+          settings = mozilla-common-settings;
         };
 
         package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
@@ -114,11 +117,13 @@ in {
             Bookmarks = [ ];
             "Extensions" = {
               "Install" = [
-                "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin"
-                "https://addons.mozilla.org/firefox/downloads/latest/tridactyl-vim"
-                "https://addons.mozilla.org/firefox/downloads/latest/umatrix"
                 "https://addons.mozilla.org/firefox/downloads/latest/browserpass-ce"
+                "https://addons.mozilla.org/firefox/downloads/latest/containerise"
                 "https://addons.mozilla.org/firefox/downloads/latest/decentraleyes"
+                "https://addons.mozilla.org/firefox/downloads/latest/tridactyl-vim"
+                "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin"
+                "https://addons.mozilla.org/firefox/downloads/latest/umatrix"
+                "https://addons.mozilla.org/firefox/downloads/latest/windows-xp-internet-browser"
               ];
             };
             ExtensionSettings = {
@@ -132,56 +137,7 @@ in {
             SearchEngines = {
               Default = "DuckDuckGo";
               Remove = [ "Google" "Bing" "Amazon.com" ];
-              Add = [
-                {
-                  "Name" = "Invidious";
-                  "Description" =
-                    "Search for videos, channels, and playlists on Invidious";
-                  "URLTemplate" =
-                    "https://invidious.snopyta.org/search?q={searchTerms}";
-                  "Method" = "GET";
-                  "IconURL" = "https://invidious.snopyta.org/favicon.ico";
-                  "Alias" = "invidious";
-                }
-                {
-                  "Name" = "GitHub";
-                  "Description" = "Search GitHub";
-                  "URLTemplate" = "https://github.com/search?q={searchTerms}";
-                  "Method" = "GET";
-                  "IconURL" = "https://github.com/favicon.ico";
-                  "Alias" = "github";
-                }
-                {
-                  "Name" = "Wikipedia (es)";
-                  "Description" = "Wikipedia (es)";
-                  "URLTemplate" =
-                    "https://es.wikipedia.org/w/api.php?action=opensearch&amp;format=xml&amp;search={searchTerms}&amp;namespace=100|104|0";
-                  "Method" = "GET";
-                  "IconURL" =
-                    "https://es.wikipedia.org/static/favicon/wikipedia.ico";
-                  "Alias" = "wikipedia-es";
-                }
-                {
-                  "Name" = "NixOS options";
-                  "Description" =
-                    "Search NixOS options by name or description.";
-                  "URLTemplate" =
-                    "https://search.nixos.org/options?query={searchTerms}";
-                  "Method" = "GET";
-                  "IconURL" = "https://nixos.org/favicon.png";
-                  "Alias" = "nixos-options";
-                }
-                {
-                  "Name" = "NixOS packages";
-                  "Description" =
-                    "Search NixOS options by name or description.";
-                  "URLTemplate" =
-                    "https://search.nixos.org/packages?query={searchTerms}";
-                  "Method" = "GET";
-                  "IconURL" = "https://nixos.org/favicon.png";
-                  "Alias" = "nixos-packages";
-                }
-              ];
+              Add = [ ];
             };
           };
         };
