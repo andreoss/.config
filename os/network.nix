763c7c34346c0c9ee768581318be820cfebb87c8
andreoss <andreoss@sdf.org>
2023-12-24 01:26:39 -0500

    refine network interface settings

--- os/network.nix
+++ os/network.nix
@@ -70,9 +70,13 @@ in {
   services.kea.dhcp4 = {
     enable = true;
     settings = {
+      lease-database = {
+        type = "memfile";
+        persist = false;
+      };
       interfaces-config = { interfaces = [ "virbr0" ]; };
       subnet4 = [{
-        pools = [{ pool = "203.0.113.100 - 203.0.113.240"; }];
+        pools = [{ pool = "203.0.113.100 - 203.0.113.250"; }];
         subnet = "203.0.113.0/24";
       }];
       option-data = [{
