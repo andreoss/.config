c4e48fb64fd68566f6ebfd4d1a0b6fee1082f21d
andreoss <andreoss@sdf.org>
2024-01-14 17:22:49 -0500

    grub efi config

---
+++ os/boot-grub-efi.nix
@@ -0,0 +1,24 @@
+{ lib, config, pkgs, ... }: {
+  config = {
+    boot.loader = {
+      efi.efiSysMountPoint = "/boot/efi";
+      efi.canTouchEfiVariables = false;
+      grub = {
+        configurationLimit = 3;
+        devices = [ "nodev" ];
+        efiInstallAsRemovable = true;
+        efiSupport = true;
+        enableCryptodisk = true;
+        enable = true;
+        font =
+          "${pkgs.terminus_font_ttf}/share/fonts/truetype/TerminusTTF-Bold.ttf";
+        fontSize = 36;
+        gfxmodeBios = "1920x1080";
+        gfxmodeEfi = "1920x1080";
+        splashImage = ../wp/1.jpeg;
+        splashMode = "stretch";
+        theme = null;
+      };
+    };
+  };
+}
