8c5eb578aaae0a2415b2c117949109529a074e26
andreoss <andreoss@sdf.org>
2024-01-17 02:12:36 -0500

    swap

--- os/btrfs-swap.nix
+++ os/btrfs-swap.nix
@@ -6,13 +6,16 @@
           mem = pkgs.runCommand "meminfo.nix" { } ''
             ${pkgs.gawk.out}/bin/awk -F'[: ]+' 'BEGIN {print "{\n";} /Mem/{ printf "\t\"%s\" = %d;\n", $1, $2; }; END {print "}";}' /proc/meminfo > "$out"'';
           memInfo = import "${mem.out}";
-          swapFile = "/nix/var/tmp/swap";
+          swapFile = "/nix/var/swap";
         in {
           serviceConfig.Type = "oneshot";
           wantedBy = [ "nix-var-swap.swap" ];
           script = ''
-            ${pkgs.util-linux}/bin/swapoff ${swapFile}
-            ${pkgs.coreutils}/bin/rm --force ${swapFile}
+            if [ -e ${swapFile} ]
+            then
+              ${pkgs.util-linux}/bin/swapoff ${swapFile}
+              ${pkgs.coreutils}/bin/rm --force ${swapFile}
+            fi
             ${pkgs.btrfs-progs}/bin/btrfs filesystem mkswapfile --size ${
               builtins.toString memInfo.MemTotal
             }K ${swapFile}
