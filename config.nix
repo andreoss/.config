18a0bd6dafe195a8d6d4c9a436b78ac9e453013b
andreoss <andreoss@sdf.org>
2024-02-24 21:16:45 -0500

    update config.nix

--- config.nix
+++ config.nix
@@ -26,7 +26,7 @@ with lib; {
 
       handle = "andreoss";
       email = "andreoss@sdf.org";
-      key = "57CC46231DA832A8";
+      key = "1234C39AACB241B1";
       keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCaGxzjPFUqy3uhXw3RgOGgMrvbJBJCpN5/ckx2D92+rsMxH9FdeZWqtXSKe3Ra1lzfyrn7NUSab2ly8/aSpgtG0lUxkKWBPuaR4OBEqpq4ODlafjuLooyXp7OLZWhXdTutqSMjAi8H+rtUryGpQhFsYV171nVUVOf/Sychmir/GIquZI6du1C19P9tFD+BaKqBzOty3z3lr4L/3MOVcWjoOFL2SPcdCIx0rPl6H+MqxjTc1h5CIos2h5feDjLJnfG9+xt2eGe8xu0aVNCKzwEqO47hYN6uLO9Y7ScnFj5/TDD+K7FJkyH9Ly0hZtM8YWhfBVVzrKM7zFjA4D52ffjeGkt0z0zWXWxNLWNLvM2xVIlYMcWwdwNdzf4OYGljCjLXh8Za7v9ZszURZ3JkRwN/4ZAlqgXLK4Vk2NMS3RhvY/b7Vi1TGhw8NMfX+6LReLG4NOoX2Uyiq5Dh2s2dFFOYsu+S1FvYyulih3POWePn2lF0Gj3lheQWllc9J38chdM= a"
       ];
