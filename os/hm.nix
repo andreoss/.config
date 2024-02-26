78dc3bf245d9b1e3cc045fd0d8ca54fb7f83628b
andreoss <andreoss@sdf.org>
2024-02-26 01:54:49 -0500

    Refactor home manager modules

--- os/hm.nix
+++ os/hm.nix
@@ -1,41 +1,47 @@
-{ specialArgs, config, ... }: {
+{ specialArgs, lib, pkgs, config, ... }: {
   home-manager.extraSpecialArgs = specialArgs;
   home-manager.users.root = {
-    home.stateVersion = config.ao.stateVersion;
-    imports = [ ../config.nix ./hm-root.nix ];
+    home.stateVersion = config.stateVersion;
+    imports = [{
+      home.packages = with pkgs; [ nvi pciutils usbutils ];
+      home.sessionVariables = { NIX_SHELL_PRESERVE_PROMPT = 1; };
+    }];
   };
-  home-manager.users."${config.ao.primaryUser.name}" = {
+  home-manager.users."${config.primaryUser.name}" = {
     nixpkgs.overlays = specialArgs.overlays;
-    home.stateVersion = config.ao.stateVersion;
+    home.stateVersion = config.stateVersion;
     imports = [
+      ../default.nix
+      specialArgs.inputs.emacs-d.nixosModules.home-manager
       ../modules/development
       ../modules/multimedia.nix
       ../modules/web.nix
       ../modules/office.nix
       ../hm/base.nix
       ../hm/home.nix
-      ../hm/mail.nix
       ../hm/sh.nix
       ../hm/term.nix
       ../hm/xsession-base.nix
-      specialArgs.inputs.emacs-d.nixosModules.home-manager
       ../hm/xsession.nix
-      ../hm/work.nix
-      {
-        home.development = {
-          cxx.enable = true;
-          haskell.enable = true;
-          java.enable = true;
-          js.enable = true;
-          lisp.enable = true;
-          perl.enable = true;
-          rust.enable = true;
-          scala.enable = true;
-        };
-      }
-      { home.multimedia.enable = true; }
-      { home.web.enable = true; }
-      { home.office.enable = true; }
-    ];
+    ] ++ (lib.optionals (builtins.elem "work" config.features)
+      [ ../hm/work.nix ])
+      ++ (lib.optionals (builtins.elem "email" config.features)
+        [ ../hm/mail.nix ]) ++ [
+          {
+            home.development = {
+              cxx.enable = true;
+              haskell.enable = true;
+              java.enable = true;
+              js.enable = true;
+              lisp.enable = true;
+              perl.enable = true;
+              rust.enable = true;
+              scala.enable = true;
+            };
+          }
+          { home.multimedia.enable = true; }
+          { home.web.enable = true; }
+          { home.office.enable = true; }
+        ];
   };
 }
