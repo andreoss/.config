bc5a90c631996b7aea2bba7f23544bea7e314e8c
andreoss <andreoss@sdf.org>
2024-03-04 09:37:51 -0500

    Refactor console and ISO configs

---
+++ os/console.nix
@@ -0,0 +1,27 @@
+{ lib, pkgs, config, modulesPath, ... }:
+let palette = import ./palette.nix;
+in {
+  console = {
+    packages = [ pkgs.uw-ttyp0 ];
+    font = "t0-18b-uni";
+    colors = builtins.map (x: builtins.replaceStrings [ "#" ] [ "" ] x)
+      (with palette; [
+        black1
+        red1
+        green1
+        yellow1
+        blue1
+        red3
+        cyan1
+        white3
+        black2
+        orange1
+        gray1
+        gray2
+        gray3
+        magenta
+        red3
+        white1
+      ]);
+  };
+}
