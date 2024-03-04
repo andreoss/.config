26e23c75d266136ff2983c94d37aa212732c1889
andreoss <andreoss@sdf.org>
2024-03-04 15:02:35 -0500

    Update flake and boot references

--- flake.nix
+++ flake.nix
@@ -216,7 +216,7 @@
           config.minimalInstallation = false;
           config.autoLogin = true;
           config.preferPipewire = true;
-          config.features = [ "vm" "multimedia" "web" "office" ];
+          config.features = [ "multimedia" "web" "office" ];
 
         };
         modules = [
