b11e1e1391852d597dd59d9f89540f9d5a9e5968
andreoss <andreoss@sdf.org>
2024-03-30 21:43:29 -0400

    fix deprecated name

--- hm/sh.nix
+++ hm/sh.nix
@@ -28,7 +28,7 @@
         defaultKeymap = "viins";
         autocd = true;
         enableCompletion = true;
-        enableAutosuggestions = true;
+        autosuggestion.enable = true;
         zplug = {
           enable = true;
           plugins = [{ name = "nnao45/zsh-kubectl-completion"; }];
