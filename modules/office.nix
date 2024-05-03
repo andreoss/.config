41ad5bf200c9d19393d11c33f5c0816afb2f7132
andreoss <andreoss@sdf.org>
2024-05-03 11:13:24 -0400

    Simplify office module configuration

--- modules/office.nix
+++ modules/office.nix
@@ -1,14 +1,8 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ config, pkgs, lib, ... }:
 let
   palette = import ../os/palette.nix;
   cfg = config.home.office;
-in
-{
+in {
   imports = [ ];
   options = {
     home.office = with lib; {
@@ -58,7 +52,6 @@ in
     home = {
       packages = with pkgs; [
         abiword
-        calibre
         ditaa
         djview
         dmtx-utils
