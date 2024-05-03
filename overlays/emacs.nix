5dd43a34c1f47aa8f658f81393cd3c1b6a6dd001
andreoss <andreoss@sdf.org>
2024-05-03 11:10:22 -0400

    Remove emacs overlay definition

--- overlays/emacs.nix
+++
@@ -1,9 +0,0 @@
-final: prev: {
-  emacs = (prev.pkgs.emacs.override {
-    withToolkitScrollBars = false;
-    withAthena = true;
-    withXwidgets = false;
-    nativeComp = true;
-    enableParallelBuilding = false;
-  }).overrideAttrs( old : {} );
-}
