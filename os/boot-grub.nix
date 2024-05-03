029eaebc135e171b5b5162ddb57fa7889692721f
andreoss <andreoss@sdf.org>
2024-05-03 11:13:24 -0400

    Simplify grub boot configuration

--- os/boot-grub.nix
+++ os/boot-grub.nix
@@ -1,10 +1,4 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}:
-{
+{ lib, config, pkgs, ... }: {
   config = {
     boot.loader = {
       grub = {
@@ -18,7 +12,8 @@
         gfxmodeBios = "1920x1080";
         gfxmodeEfi = "1920x1080";
         fontSize = 36;
-        font = "${pkgs.terminus_font_ttf}/share/fonts/truetype/TerminusTTF-Bold.ttf";
+        font =
+          "${pkgs.terminus_font_ttf}/share/fonts/truetype/TerminusTTF-Bold.ttf";
       };
     };
   };
