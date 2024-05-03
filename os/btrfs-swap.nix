cc3bc5a39bce0672734989f297d6b9e0a18e80ac
andreoss <andreoss@sdf.org>
2024-05-03 11:13:24 -0400

    Update btrfs swap configuration

--- os/btrfs-swap.nix
+++ os/btrfs-swap.nix
@@ -1,28 +1,35 @@
-{ lib, pkgs, modulesPath, ... }: {
+{
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
+{
   config = {
     systemd = {
       services = {
-        create-swapfile = let
-          mem = pkgs.runCommand "meminfo.nix" { } ''
-            ${pkgs.gawk.out}/bin/awk -F'[: ]+' 'BEGIN {print "{\n";} /Mem/{ printf "\t\"%s\" = %d;\n", $1, $2; }; END {print "}";}' /proc/meminfo > "$out"'';
-          memInfo = import "${mem.out}";
-          swapFile = "/nix/var/swap";
-        in {
-          serviceConfig.Type = "oneshot";
-          wantedBy = [ "nix-var-swap.swap" ];
-          script = ''
-            if [ -e ${swapFile} ]
-            then
-              ${pkgs.util-linux}/bin/swapoff ${swapFile}
-              ${pkgs.coreutils}/bin/rm --force ${swapFile}
-            fi
-            ${pkgs.btrfs-progs}/bin/btrfs filesystem mkswapfile --size ${
-              builtins.toString memInfo.MemTotal
-            }K ${swapFile}
-          '';
-        };
+        create-swapfile =
+          let
+            mem =
+              pkgs.runCommand "meminfo.nix" { }
+                ''${pkgs.gawk.out}/bin/awk -F'[: ]+' 'BEGIN {print "{\n";} /Mem/{ printf "\t\"%s\" = %d;\n", $1, $2; }; END {print "}";}' /proc/meminfo > "$out"'';
+            memInfo = import "${mem.out}";
+            swapFile = "/nix/var/swap";
+          in
+          {
+            serviceConfig.Type = "oneshot";
+            wantedBy = [ "nix-var-swap.swap" ];
+            script = ''
+              if [ -e ${swapFile} ]
+              then
+                ${pkgs.util-linux}/bin/swapoff ${swapFile}
+                ${pkgs.coreutils}/bin/rm --force ${swapFile}
+              fi
+              ${pkgs.btrfs-progs}/bin/btrfs filesystem mkswapfile --size ${builtins.toString memInfo.MemTotal}K ${swapFile}
+            '';
+          };
       };
     };
-    swapDevices = [{ device = "/nix/var/swap"; }];
+    swapDevices = [ { device = "/nix/var/swap"; } ];
   };
 }
