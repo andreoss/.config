432a472e24433f5774a98ab16039e0696bf5cc2b
andreoss <andreoss@sdf.org>
2024-05-03 11:13:24 -0400

    Simplify home manager root

--- os/hm-root.nix
+++ os/hm-root.nix
@@ -1,17 +1,4 @@
-{
-  config,
-  pkgs,
-  lib,
-  stdenv,
-  ...
-}:
-{
-  home.packages = with pkgs; [
-    nvi
-    pciutils
-    usbutils
-  ];
-  home.sessionVariables = {
-    NIX_SHELL_PRESERVE_PROMPT = 1;
-  };
+{ config, pkgs, lib, stdenv, ... }: {
+  home.packages = with pkgs; [ nvi pciutils usbutils ];
+  home.sessionVariables = { NIX_SHELL_PRESERVE_PROMPT = 1; };
 }
