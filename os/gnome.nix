22f05c6175c821319adc7a02ce8f2f461e075ac0
andreoss <andreoss@sdf.org>
2024-05-03 11:10:15 -0400

    Simplify gnome desktop setup

--- os/gnome.nix
+++ os/gnome.nix
@@ -1,4 +1,9 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   services.gnome.gnome-initial-setup.enable = false;
   services.gnome.gnome-user-share.enable = false;
@@ -24,36 +29,36 @@
     gnome-disk-utility
   ];
   services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
-        [org.gnome.desktop.background]
-        picture-uri="${../wp/1.jpeg}"
-        picture-options='centered'
-        primary-color='#3b77bc'
-        secondary-color='#023c88'
-        [org.gnome.desktop.sound]
-        event-sounds=false
-        [org.gnome.desktop.privacy]
-        disable-microphone=true
-        hide-identity=true
-        recent-files-max-age=0
-        remove-old-temp-files=true
-        remember-app-usage=false
-        disable-camera=true
-        remember-recent-files=false
-        report-technical-problems=false
-        remove-old-trash-files=true
-        show-full-name-in-top-bar=false
-        send-software-usage-stats=false
-        [org.gnome.desktop.default-applications.terminal]
-        exec-arg="-x"
-        exec="urxvt"
-        [org.gnome.mutter]
-        dynamic-workspaces=true
-        center-new-windows=true
-        [org.gnome.nautilus.preferences]
-        automatic-decompression=false
-        sort-directories-first=true
-        [org.gtk.settings.file-chooser]
-        sort-directories-first=true
-        location-mode='path-bar'
+    [org.gnome.desktop.background]
+    picture-uri="${../wp/1.jpeg}"
+    picture-options='centered'
+    primary-color='#3b77bc'
+    secondary-color='#023c88'
+    [org.gnome.desktop.sound]
+    event-sounds=false
+    [org.gnome.desktop.privacy]
+    disable-microphone=true
+    hide-identity=true
+    recent-files-max-age=0
+    remove-old-temp-files=true
+    remember-app-usage=false
+    disable-camera=true
+    remember-recent-files=false
+    report-technical-problems=false
+    remove-old-trash-files=true
+    show-full-name-in-top-bar=false
+    send-software-usage-stats=false
+    [org.gnome.desktop.default-applications.terminal]
+    exec-arg="-x"
+    exec="urxvt"
+    [org.gnome.mutter]
+    dynamic-workspaces=true
+    center-new-windows=true
+    [org.gnome.nautilus.preferences]
+    automatic-decompression=false
+    sort-directories-first=true
+    [org.gtk.settings.file-chooser]
+    sort-directories-first=true
+    location-mode='path-bar'
   '';
 }
