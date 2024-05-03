997ad329fccc172968c4bd499495a5db287b4606
andreoss <andreoss@sdf.org>
2024-05-03 11:13:24 -0400

    Expand xserver module configuration

--- os/xserver.nix
+++ os/xserver.nix
@@ -1,11 +1,4 @@
-{
-  lib,
-  config,
-  pkgs,
-  self,
-  ...
-}:
-{
+{ lib, config, pkgs, self, ... }: {
   config = {
     services.startx = {
       enable = lib.mkForce config.autoLogin;
@@ -19,7 +12,8 @@
         enable = true;
         time = config.autoLock.time;
         extraOptions = [ "-detectsleep" ];
-        notifier = ''${pkgs.libnotify}/bin/notify-send "Блокировка через 10 секунд"'';
+        notifier =
+          ''${pkgs.libnotify}/bin/notify-send "Блокировка через 10 секунд"'';
         locker = "/run/wrappers/bin/physlock";
         enableNotifier = true;
       };
@@ -28,20 +22,41 @@
         touchpad.naturalScrolling = true;
         touchpad.tapping = true;
       };
-      inputClassSections = [
-        ''
-          Identifier     "TrackPoint configuration"
-          MatchProduct   "TrackPoint"
-          Option         "AccelSpeed" "0.6"
-        ''
-      ];
+      inputClassSections = [''
+        Identifier     "TrackPoint configuration"
+        MatchProduct   "TrackPoint"
+        Option         "AccelSpeed" "0.6"
+      ''];
     };
-    environment = {
-      etc = {
-        "icewm" = {
-          source = ../icewm;
+    fonts = {
+      fontDir = { enable = true; };
+      enableDefaultPackages = true;
+      enableGhostscriptFonts = true;
+      packages = with pkgs; [
+        _3270font
+        fixedsys-excelsior
+        hermit
+        julia-mono
+        maple-mono
+        terminus_font
+        terminus_font_ttf
+        ucs-fonts
+        uw-ttyp0
+        spleen
+      ];
+      fontconfig = {
+        hinting.enable = true;
+        hinting.autohint = true;
+        hinting.style = "full";
+        useEmbeddedBitmaps = true;
+        defaultFonts = {
+          monospace = [ "Terminus" ];
+          sansSerif = [ "Terminus" ];
+          serif = [ "Terminus" ];
+          emoji = [ "Unifont" ];
         };
       };
     };
+    environment = { etc = { "icewm" = { source = ../icewm; }; }; };
   };
 }
