e1117a7b814073c6db22051bfe5c5b9e9af1c062
andreoss <andreoss@sdf.org>
2024-05-03 01:12:41 -0400

    Simplify users configuration module

--- os/users.nix
+++ os/users.nix
@@ -20,7 +20,6 @@
   users.groups.disk.members = [ config.primaryUser.name ];
   services.logind.killUserProcesses = true;
   services.logind.lidSwitch = "suspend";
-  services.logind.lidSwitchExternalPower = "lock";
   services.logind.extraConfig = "";
   programs.bash = {
     promptInit = ''
