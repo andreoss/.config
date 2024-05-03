3da5b5c545ebe5970a0bc505af609a026698fcd7
andreoss <andreoss@sdf.org>
2024-05-03 11:13:24 -0400

    Update boot loader configuration

--- os/boot-loader.nix
+++ os/boot-loader.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   boot.loader = {
     efi.canTouchEfiVariables = false;
     systemd-boot = {
@@ -7,7 +8,6 @@
       extraInstallCommands = with pkgs; ''
         ${perl536}/bin/perl -i -pE 's/^title \s* N.*/title X/x; s/^version \s+ (\S+) \s+ (\S+).*/version $2/x' /boot/loader/entries/*
       '';
-
     };
   };
 }
